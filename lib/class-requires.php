ef36b42b2c8ff5f4d072a66f1f232b98c70d865f
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-01-06 03:07:41 -0500

    Update to lib-requires

---
+++ lib/class-requires.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Assets Compiler Functions
+ *
+ * Migrated out of DDP project.
+ *
+ * @author peshkov@UD
+ * @version 0.1.2
+ * @module UsabilityDynamics
+ */
+namespace UsabilityDynamics {
+
+  if( !class_exists( 'UsabilityDynamics\Requires' ) ) {
+
+    /**
+     * Class Requires
+     *
+     * @class Requires
+     */
+    class Requires {
+
+      /**
+       * Constructor.
+       * All pathes to libraries must be defined here.
+       * Should be called before init, because it adds specific hooks
+       *
+       * @param mixed $args
+       *
+       * @author peshkov@UD
+       */
+      function __construct( $args = array() ) {
+
+      }
+
+    }
+
+  }
+
+}
